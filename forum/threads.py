7f5336ab34e970f902521fb86f6d8e31edd2d17c
__SyS__ <sreyassreelal@gmail.com>
2018-05-25 19:02:30 +0530

    added thread class functions

--- forum/threads.py
+++ forum/threads.py
@@ -1,21 +1,35 @@
-
 class Thread:
-    def __init__(self,id):
+    def __init__(self,client,id):
+        self.id = id
+        self.client = client
+        self.client.get("http://forum.sa-mp.com/showthread.php?t=" + self.id)
         self.author = self.__getauthor()
         self.title = self.__gettitle()
-
+        
     def __getauthor(self):
-        pass
+        from forum.user import User
+        self.client.get("http://forum.sa-mp.com/showthread.php?t=" + self.id)
+        return User(self.client,self.client.find_element_by_xpath("//*[contains(@class, 'bigusername')]").get_attribute('href')[36:])
     
     def __gettitle(self):
-        pass
+        self.client.get("http://forum.sa-mp.com/showthread.php?t=" + self.id)
+        return self.client.find_element_by_tag_name('strong').text
 
-    def getreplycount(self):
-        pass
 
     def getrating(self):
-        pass
-    
-    def getsubforum(self):
-        pass
+        try:
+            self.client.get("http://forum.sa-mp.com/showthread.php?t=" + self.id)
+            rating_info = self.client.find_element_by_xpath("//*[starts-with(@src, 'images/rating/rating_')]").get_attribute('title')
+            rating = {}
+            rating['NoOfVotes'] = rating_info[rating_info.find(':')+2:rating_info.find('v')-1]
+            rating['Average'] = rating_info[rating_info.find(',')+2:rating_info.find('aver')-1]
+            return rating
+        except:
+            return {'NoOfVotes':'0','Average':'0'}
 
+    def getsubforum(self):
+        sub_forums_elements = self.client.find_elements_by_xpath("//*[starts-with(@href, 'forumdisplay.php?f=')]")
+        sub_forums = []
+        for e in sub_forums_elements:
+            sub_forums.append(e.text)
+        return sub_forums
