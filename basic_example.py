fc4ef48940792ad89828b7bec064b2633cdb6b62
__SyS__ <sreyassreelal@gmail.com>
2018-05-26 13:19:50 +0530

    updated basic example with newer functions

--- basic_example.py
+++ basic_example.py
@@ -1,22 +1,22 @@
-from forum.ext.driver import client
 from forum.account import Account
 
 name = input("Input your forum user name : ")
 password = input("Input your forum password : ")
+a = Account(name,password)
+print("Logined with user id : ",a.id)
+print("\t**Account info**\n",a.User.info())
 
-a = Account(client,name,password)
+print("\n\t***Contacts***\n")
+print("\tId\tUsername")
 
-print("Account info\n",a.User.info())
+for i in a.getcontacts():
+    print("\t",i.id,"\t",i.name)
 
-print("\t***Threads***\n")
+print("\n\t***Threads***\n")
 print("\tTitle\tRatings\tAuthor")
 
 for i in a.User.getthreads():
     print(i.title,"\t",i.getrating(),"\t",i.author.name)
 
-print("\t***Contacts***\n")
-print("\tId\tUsername")
 
-for i in a.getcontacts():
-    print("\t",i.id,"\t",i.name)
 
