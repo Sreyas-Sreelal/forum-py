4640443fe31c8a69cc4e92d93a5226765b878494
__SyS__ <sreyassreelal@gmail.com>
2018-05-26 09:59:56 +0530

    added max login limit exception

--- forum/ext/errors.py
+++ forum/ext/errors.py
@@ -13,3 +13,9 @@ class DriverLoadError(Error):
     Raised when phantom JS driver is not found
     """
     pass
+
+class MaxLoginLimit(Error):
+    """
+    Raised when forum login limit is reached (wait for 15 minutes before attempting another login)
+    """
+    pass
