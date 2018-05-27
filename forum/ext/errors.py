f25fa12845e94b6739d7253d37f03ee91a54d805
__SyS__ <sreyassreelal@gmail.com>
2018-05-27 15:05:14 +0530

    added MustLogin Exception

--- forum/ext/errors.py
+++ forum/ext/errors.py
@@ -19,3 +19,9 @@ class MaxLoginLimit(Error):
     Raised when forum login limit is reached (wait for 15 minutes before attempting another login)
     """
     pass
+
+class MustLogin(Error):
+    """
+    Raised when a non-logged in attempt to process a request that needs a logged in account
+    """
+    pass
