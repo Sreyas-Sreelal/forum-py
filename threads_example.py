5b9910ca133556c1a84eeadaf694f160d04fd590
__SyS__ <sreyassreelal@gmail.com>
2018-05-27 15:07:38 +0530

    added threads example to illustrate the usage of Threads

---
+++ threads_example.py
@@ -0,0 +1,8 @@
+from forum.threads import Thread
+
+t = Thread(input("Input thread id : "))
+"""print("*****Content*****\n",t.getcontent())
+print("Author: ",t.author.name)
+"""
+for posts in t.getposts():
+    print(posts)
