3356ca63e4fdb2809660aa0bc61f2c46281996a3
__SyS__ <sreyassreelal@gmail.com>
2018-05-27 15:09:42 +0530

    updated threads example

--- threads_example.py
+++ threads_example.py
@@ -1,8 +1,8 @@
 from forum.threads import Thread
 
 t = Thread(input("Input thread id : "))
-"""print("*****Content*****\n",t.getcontent())
+print("*****Content*****\n",t.getcontent())
 print("Author: ",t.author.name)
-"""
+print("******Posts******\n")
 for posts in t.getposts():
     print(posts)
