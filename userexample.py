d62d4a5c00428e922463fe2f0c7c246734d81823
__SyS__ <sreyassreelal@gmail.com>
2018-05-29 10:19:17 +0530

    updated user example with recent changes

--- userexample.py
+++ userexample.py
@@ -5,13 +5,15 @@ name = input("Input forum username : ")
 password = input("Input forum password : ")
 a = Account(name,password)
 
-id = input("Input user id ")
+id = input("Input user id : ")
 u = User(id)
 info = u.info()
 
+print("Name : ",u.name)
+print("Forum level : ",u.getforumlevel())
 for i,j in info.items():
     print(i," : ",j)
-print("User ",u.name," has reputation ",u.getreputation())
+print("Reputation : ",u.getreputation())
 print("Last online : ",u.getlastactive(a))
 
 
