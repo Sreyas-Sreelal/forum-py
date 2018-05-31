d47072895f115d20732357918aa29721608cd917
__SyS__ <sreyassreelal@gmail.com>
2018-05-31 13:58:53 +0530

    updated pms_example with lastest feature

--- pms_example.py
+++ pms_example.py
@@ -6,3 +6,13 @@ password = input("Input forum password : ")
 a = Account(name,password)
 print(a.getpms())
 
+rec_names = input("Input name of recipents (seperated by space) : ").split(" ")
+
+users = []
+for rec_name in rec_names:
+    users.append(User(a.getIdFromUserName(rec_name)))
+title = input("Input title : ")
+content = input("Input content : ")
+a.send_pm(users,title,content)
+
+
